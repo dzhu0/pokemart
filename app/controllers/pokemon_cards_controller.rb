9ffed4f966576908e63284638b6098b65b8c4823
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2024-05-03 16:57:52 -0500

    Add PokemonCard Filter

--- app/controllers/pokemon_cards_controller.rb
+++ app/controllers/pokemon_cards_controller.rb
@@ -2,6 +2,15 @@ class PokemonCardsController < ApplicationController
     def index
         @pokemon_cards = PokemonCard.where("stock_quantity > 0")
 
+        if params[:filter].present?
+            case params[:filter]
+            when "new"
+                @pokemon_cards = @pokemon_cards.where("created_at >= ?", 3.days.ago)
+            when "recently_updated"
+                @pokemon_cards = @pokemon_cards.where("updated_at >= ? AND created_at < ?", 3.days.ago, 3.days.ago)
+            end
+        end
+
         @pokemon_cards = @pokemon_cards.page(params[:page]).per(15)
     end
 end
