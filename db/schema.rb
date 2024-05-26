4bfb1046c0586aa7ab9111d0c3a6d55a35400452
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2024-05-26 17:49:00 -0500

    DB Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_30_190609) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_26_222121) do
+  create_table "abouts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
