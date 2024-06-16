bd93f9e0b860f1026d957f97ccd0a1bb6ebad37e
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2024-06-15 20:15:07 -0500

    Add Fields to Customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,9 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  belongs_to :province, optional: true
+
+  validates :username, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9_-]{3,15}\z/, message: "should be 3 to 15 characters long and can only contain letters, digits, underscores, and hyphens" }
+  validates :email, format: { with: /\A\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+\z/, message: "is invalid" }
 end
