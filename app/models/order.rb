3e37f8726c7592f40e757d1f00cd5713feb1c70a
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2024-06-30 20:34:26 -0500

    Add Validation For Order and OrderDetail

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,8 @@ class Order < ApplicationRecord
     belongs_to :province
 
     has_many :order_details
+
+    validates :customer_id, presence: true
+    validates :province_id, presence: true
+    validates :address, presence: true
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,9 @@
 class OrderDetail < ApplicationRecord
     belongs_to :order
     belongs_to :pokemon_card
+
+    validates :order_id, presence: true
+    validates :pokemon_card_id, presence: true
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
+    validates :quantity, presence: true, numericality: { greater_than: 0 }
 end
