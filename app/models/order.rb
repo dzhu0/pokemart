918abd2eeb2cdd823a4bae51932cbe972f5de61f
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2024-07-01 19:34:51 -0500

    Add Instance Methods for Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,32 @@ class Order < ApplicationRecord
     validates :customer_id, presence: true
     validates :province_id, presence: true
     validates :address, presence: true
+
+    def total_quantity
+        order_details.sum { |item| item.quantity }
+    end
+
+    def subtotal
+        order_details.sum { |item| item.quantity * item.price }
+    end
+
+    def tax_history
+        province.tax_histories.where("created_at <= ?", created_at).order(created_at: :desc).first
+    end
+
+    def pst
+        subtotal * (tax_history&.pst || 0) / 100
+    end
+
+    def gst
+        subtotal * (tax_history&.gst || 0) / 100
+    end
+
+    def hst
+        subtotal * (tax_history&.hst || 0) / 100
+    end
+
+    def total
+        subtotal + pst + gst + hst
+    end
 end
